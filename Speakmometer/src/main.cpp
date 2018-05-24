8d3d9dc79b8004e0968d451d24e38dffbb9ee0cb
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-05-24 20:22:26 +0700

    speak temp

--- Speakmometer/src/main.cpp
+++ Speakmometer/src/main.cpp
@@ -1,3 +1,13 @@
+/*
+
+
+Download mp3 file form youtube --> https://ycapi.org/iframe/
+Convert mp3 to wav --> https://audio.online-convert.com/convert-to-wav
+    Resolution 8 bit
+    sampling rate 16000 Hz (16 khz)
+    audio channels mono
+
+*/
 #include <Arduino.h>
 #include <SPI.h>
 #include <SD.h>
@@ -18,10 +28,10 @@ int t;
 String lang;
 String m_str;
 
-
 void setup()
 {
     Serial.begin(57600);
+    Serial.println("Serial done.");
 
     if (!SD.begin(SD_ChipSelectPin))
     {
@@ -36,10 +46,15 @@ void setup()
 
     tmrpcm.speakerPin = 9;
     tmrpcm.setVolume(6);
-    tmrpcm.play("temp.wav");
-    delay(5000);
-    tmrpcm.play("tellTemp1.mp3");
-    delay(1000);
+    tmrpcm.quality(1);
+    // tmrpcm.play("temp.wav", 2);
+    // delay(5000);
+    // tmrpcm.play("tell1.wav", 3);
+    // delay(5000);
+    // tmrpcm.play("tell2.wav", 3);
+    // delay(5000);
+    // tmrpcm.play("tell3.wav", 3);
+    // delay(5000);
 }
 
 void loop()
@@ -52,7 +67,13 @@ void loop()
     tmrpcm.quality(1);
     m_str = "" + String(t) + ".wav";
     char *m_play = &m_str[0u];
+
+    tmrpcm.play("tell1.wav");
+    delay(3000);
     tmrpcm.play(m_play);
+    delay(2000);
+    tmrpcm.play("celsius.wav");
+    delay(3000);
 
     Serial.print("m_play = ");
     Serial.println(m_play);
