5586d90d3b3f1401b629f09e72aebb5fc92c229c
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-05-30 18:20:05 +0700

    add sound level

--- Speakmometer/src/main.cpp
+++ Speakmometer/src/main.cpp
@@ -14,6 +14,16 @@ Convert mp3 to wav --> https://audio.online-convert.com/convert-to-wav
 #include <TMRpcm.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <LiquidCrystal.h>
+
+#define RS 8
+#define EN 7
+#define D4 A0
+#define D5 A1
+#define D6 A2
+#define D7 A3
+
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 #define SD_ChipSelectPin 10
 
@@ -38,6 +48,12 @@ void setup()
     Serial.begin(57600);
     Serial.println("Serial done.");
 
+    lcd.begin(8, 2);
+    lcd.setCursor(0, 0);
+    lcd.print("Thermo  ");
+    lcd.setCursor(0, 1);
+    lcd.print("   Meter");
+
     if (!SD.begin(SD_ChipSelectPin))
     {
         Serial.println("SD fail");
@@ -51,12 +67,21 @@ void setup()
 
     tmrpcm.speakerPin = 9;
     tmrpcm.setVolume(5);
+
+    lcd.clear();
 }
 
 void loop()
 {
     sensors.requestTemperatures();
     temp = sensors.getTempCByIndex(0);
+
+    lcd.setCursor(0, 0);
+    lcd.print("Temp : ");
+    lcd.setCursor(0, 1);
+    lcd.print(temp);
+    lcd.print(" C  ");
+
     Serial.print("Temp = ");
     Serial.println(temp);
 
@@ -71,27 +96,48 @@ void loop()
 
     tmrpcm.quality(1);
 
-    tmrpcm.play("tell1.wav");
-    delay(3000);
-
     m_str1 = "" + String(i_temp) + ".wav";
     char *m_play1 = &m_str1[0u];
     tmrpcm.play(m_play1);
     delay(2000);
 
-    tmrpcm.play("point.wav");
-    delay(2000);
-
-    f_temp = 5.0;
     int mmm = f_temp;
     Serial.println(mmm);
-    m_str2 = "" + String(mmm) + ".wav";
-    char *m_play2 = &m_str2[0u];
-    tmrpcm.play(m_play2);
-    delay(2000);
+
+    if (mmm != 0)
+    {
+        tmrpcm.play("point.wav");
+        delay(2000);
+
+        m_str2 = "" + String(mmm) + ".wav";
+        char *m_play2 = &m_str2[0u];
+        tmrpcm.play(m_play2);
+        delay(2000);
+    }
 
     tmrpcm.play("celsius.wav");
     delay(3000);
+
+    // case1 5s
+    // case2 6s
+    // case3 7s
+    // case4 9s
+    // case5 9s
+    if (temp < 37.5)
+    {
+        tmrpcm.play("case1.wav");
+        delay(6000);
+    }
+    else if (temp >= 37.5 && temp <= 38.5)
+    {
+        tmrpcm.play("case2.wav");
+        delay(7000);
+    }
+    else if (temp >= 37.5 && temp <= 38.5)
+    {
+        tmrpcm.play("case2.wav");
+        delay(7000);
+    }
 
     tmrpcm.disable();
     delay(5000);
